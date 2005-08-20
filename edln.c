eb0a61a7bed692d2e2ec65bdc7221711b9920c27
Jeremy Lin <jjlin@OCF.Berkeley.EDU>
2005-08-20 20:46:42 +0000

    Added check for empty symlink name. (Originally from 05/01/2004.)

--- edln.c
+++ edln.c
@@ -1,8 +1,13 @@
-/* edln - a simple readline-based program that lets you edit the target of
- *        a symbolic link.
+/* edln - a simple readline-based utility for interactively editing
+ *        the target of a symbolic link.
  *
- * Written by Jeremy Lin <jjlin@OCF.Berkeley.EDU>, Dec. 2002.
+ * Written by Jeremy Lin <jjlin@OCF.Berkeley.EDU>.
  * Placed in the public domain.
+ *
+ * Revision history:
+ * 
+ * 12/30/2002: Initial revision.
+ * 05/01/2004: Added check for empty symlink name.
  */
 
 #include <stdio.h>
@@ -15,7 +20,7 @@
 
 char buf[BUFLEN];
 
-int init_rl_line_buffer (void)
+int init_rl_line_buffer(void)
 {
     rl_replace_line(buf, 0);
     rl_point = rl_end; /* move cursor to end of line */
@@ -23,16 +28,21 @@ int init_rl_line_buffer (void)
     return 0;
 }
 
-int main (int argc, char** argv)
+void error(const char* msg)
+{
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char** argv)
 {
     char* link_name = NULL;
     char* new_target = NULL;
-    char* line = NULL;
 
     switch (argc) {
     case 3:
 	new_target = argv[2];
-	/* fall through */
+	/* FALL THROUGH */
     case 2:
 	link_name = argv[1];
 	break;
@@ -42,27 +52,31 @@ int main (int argc, char** argv)
     }
 
     /* read original target of symlink */
-    if (readlink(link_name, buf, sizeof(buf)) == -1)
+    if (readlink(link_name, buf, sizeof(buf)) < 0)
 	goto error;
 
     if (new_target == NULL) {
 	rl_extend_line_buffer(BUFLEN+1);
 	rl_pre_input_hook = &init_rl_line_buffer;
-	if ( (line = readline("New target: ")) == NULL) {
-	    fprintf(stderr, "No input received, aborting.\n");
-	    return EXIT_FAILURE;
+
+	if ( (new_target = readline("New target: ")) == NULL)
+	    error("No input received, aborting.");
+
+	if (!strcmp(new_target, buf)) {
+	    /* new target same as old target, so don't make any changes */
+	    return EXIT_SUCCESS;
+	} else if (*new_target == '\0') {
+	    /* empty target */
+	    error("Can't symlink to empty filename.");
 	}
-	/* new target == old target */
-	if (!strcmp(line, buf)) return EXIT_SUCCESS;
-	new_target = line;
     }
 
     /* remove original symlink */
-    if (unlink(link_name) == -1)
+    if (unlink(link_name) < 0)
 	goto error;
 
     /* write out new symlink */
-    if (symlink(new_target, link_name) == -1)
+    if (symlink(new_target, link_name) < 0)
 	goto error;
 
     return EXIT_SUCCESS;
